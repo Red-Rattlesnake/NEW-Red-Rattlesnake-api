ff0144dc667dcdfaef0b66f7ba3c6aaae5429386
Sean Woolard <smwoolar26@gmail.com>
2023-05-02 01:07:42 -0400

    lab 6 done

--- RedRattlesnake.Api/Program.cs
+++ RedRattlesnake.Api/Program.cs
@@ -1,12 +1,38 @@
 using RedRattlesnake.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Sqlite;
+using RedRattlesnake.Api.Security;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 var builder = WebApplication.CreateBuilder(args);
+
+string authority = builder.Configuration["Auth0:Authority"] ??
+throw new ArgumentNullException("Auth0:Authority");
+
+string audience = builder.Configuration["Auth0:Audience"] ??
+throw new ArgumentNullException("Auth0:Audience");
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddAuthentication(optoins =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(OptionsBuilderConfigurationExtensions =>
+{
+    OptionsBuilderConfigurationExtensions.Authority = authority;
+    OptionsBuilderConfigurationExtensions.Audience = audience;
+});
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("delete:catalog", policy =>
+    policy.RequireAuthenticatedUser().RequireClaim("scope", "delete:catalog"));
+});
+
 builder.Services.AddDbContext<StoreContext>(options =>
     options.UseSqlite("Data Source=../Registrar.sqlite",
     b => b.MigrationsAssembly("RedRattlesnake.Api"))
@@ -36,6 +62,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseCors();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
